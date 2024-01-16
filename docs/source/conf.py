f2690dff7a4bafea6ed71f1c68653c3d23c78880
Gwendelyn Daniels <gwendelyndaniels@GwendelynsMBP2.attlocal.net>
2024-01-16 15:24:01 -0600

    Updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Illinois Legal Aid Online Documentation'
+copyright = '2024, Illinois Legal Aid Online'
+author = 'ILAO'
 
 release = '0.1'
 version = '0.1.0'
